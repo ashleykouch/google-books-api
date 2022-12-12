30eec072305cd2aa96509d55520dceeb8725b7d6
ashleykouch <ashleykouch99@gmail.com>
2022-12-12 18:06:24 +1100

    updated project

--- google-books/src/components/MainPage/BookCard/BookCard.jsx
+++ google-books/src/components/MainPage/BookCard/BookCard.jsx
@@ -1,21 +1,44 @@
 import styles from "./BookCard.module.scss";
+import CardInfo from "../CardInfo/CardInfo";
 
 // create a card with placeholder styles to see what the card will look like
-const BookCard = () => {
+const BookCard = ({ books }) => {
+  console.log(books);
   return (
-    <div className={styles.Card}>
-      <img
-        className={styles.Card_Img}
-        src="https://m.media-amazon.com/images/I/51zXJBloNpL._AC_SY1000_.jpg"
-        alt="book-"
-      />
-      <div className={styles.Card_Info}>
-        <h2>Book Title</h2>
-        <h3>Author</h3>
-        <p>Short Description</p>
-        <p>Price</p>
-      </div>
-    </div>
+    <>
+      {books.map((data, index) => {
+        let card =
+          data.volumeInfo.imageLinks.smallThumbnail &&
+          data.volumeInfo.imageLinks.smallThumbnail;
+
+        if (card != undefined) {
+          return (
+            <div>
+              <div key={index} className={styles.Card}>
+                <img
+                  className={styles.Card_Img}
+                  src={
+                    card ??
+                    "https://islandpress.org/sites/default/files/default_book_cover_2015.jpg"
+                  }
+                  alt="book-"
+                />
+                <div className={styles.Card_Info}>
+                  <h3 className={styles.Card_Title}>{data.volumeInfo.title}</h3>
+                  <h4>{data.volumeInfo.authors}</h4>
+                  <p className={styles.Card_Desc}>
+                    {data.volumeInfo.description}
+                  </p>
+                  <p>
+                    ${data.saleInfo?.listPrice?.amount ?? " No Price Available"}
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        }
+      })}
+    </>
   );
 };
 
--- google-books/src/components/MainPage/MainPage.jsx
+++ google-books/src/components/MainPage/MainPage.jsx
@@ -1,7 +1,22 @@
 import BookCard from "./BookCard/BookCard";
 import styles from "./MainPage.module.scss";
+import { useState } from "react";
+import axios from "axios";
+import CardInfo from "./CardInfo/CardInfo";
 
-const MainPage = () => {
+const MainPage = ({ index }) => {
+  const [search, setSearch] = useState("");
+  const [bookData, setBookData] = useState([]);
+
+  const searchBook = (event) => {
+    if (event.key === "Enter") {
+      axios
+        .get(`https://www.googleapis.com/books/v1/volumes?q=${search}+terms`)
+        .then((response) => setBookData(response.data.items))
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <>
       <div className={styles.Search}>
@@ -14,20 +29,15 @@ const MainPage = () => {
             className={styles.Search_Bar}
             type="text"
             placeholder="Search the book name here"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+            onKeyPress={searchBook}
           ></input>
           <button className={styles.Search_Button}>Search</button>
         </div>
       </div>
 
-      <div className={styles.BookCard}>
-        <BookCard />
-        <BookCard />
-        <BookCard />
-        <BookCard />
-        <BookCard />
-        <BookCard />
-        <BookCard />
-      </div>
+      <div className={styles.BookCard}>{<BookCard books={bookData} />}</div>
     </>
   );
 };
